8e7cc8e18660252f95dc7fbce478687f8a71b05c
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-08-01 10:35:07 +0200

    Fix typo

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| We bRoutes
+| Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
@@ -13,7 +13,7 @@
 
 // Authentification Routes
 Auth::routes();
-    
+
 // Global Routes
 Route::resource('home', 'HomeController');
 Route::resource('offers', 'OfferController');
